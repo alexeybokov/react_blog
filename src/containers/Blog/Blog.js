3f68fc88ecdcda19efee53bdd289e72e4f47d0b1
alexeybokov <lexabokov18@gmail.com>
2019-10-30 20:21:13 +0200

    update Blog

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,11 +8,12 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
-    }
+        posts: [],
+        selectedPostId: null
+    };
 
     componentDidMount() {
-      axios.get('https://jsonplaceholder.typicode.com/posts')
+      axios.get('https://jsonplaceholder.typicode.com/postsss')
            .then(response => {
                const posts = response.data.slice(0, 4);
                const updatedPosts = posts.map(post => {
@@ -26,11 +27,17 @@ class Blog extends Component {
            });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
+
     render () {
         const posts = this.state.posts.map(post => {
-           return <Post key={post.id}
-                        title={post.title}
-                        author={post.author} />;
+           return <Post
+               key={post.id}
+               title={post.title}
+               author={post.author}
+               clicked={() => this.postSelectedHandler(post.id) }/>;
         });
 
         return (
@@ -39,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id ={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
